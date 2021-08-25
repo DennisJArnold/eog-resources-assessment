a30d0edc6c6a8abe2df76847d32ae7732520a467
Dennis Arnold <dennis.j.arnold.iii@gmail.com>
2021-08-25 13:22:02 -0600

    Enabled storage of multiple measurements for each metric; tested and ensured they could be displayed

--- src/Features/Metrics.tsx
+++ src/Features/Metrics.tsx
@@ -6,14 +6,15 @@ import {
 import Card from '@material-ui/core/Card';
 import CardHeader from '../components/CardHeader';
 
-interface NewMeasurement {
+interface Measurement {
   metric: string,
   value: number,
+  at: number,
   unit: string
 }
 
 const METRIC_SUBSCRIPTION = gql`
-subscription OnNewMeasurement{
+subscription OnNewMeasurement {
   newMeasurement {
     metric
     at
@@ -22,18 +23,70 @@ subscription OnNewMeasurement{
   }
 }`;
 
+const oilTemp: Measurement[] = [];
+const tubingPressure: Measurement[] = [];
+const waterTemp: Measurement[] = [];
+const casingPressure: Measurement[] = [];
+const injValveOpen: Measurement[] = [];
+const flareTemp: Measurement[] = [];
+const selected: string[] = [];
+
+const currentMetrics = {
+  oilTemp,
+  tubingPressure,
+  waterTemp,
+  casingPressure,
+  injValveOpen,
+  flareTemp,
+  selected,
+};
+
 const Metrics = () => {
-  const result = useSubscription<NewMeasurement>(METRIC_SUBSCRIPTION);
+  const result = useSubscription(METRIC_SUBSCRIPTION);
   const { data, error } = result;
+
+  const updateCurrentMetrics = (newMeasurement: Measurement) => {
+    console.log(newMeasurement);
+    console.log(newMeasurement.metric);
+    switch (newMeasurement.metric) {
+      case 'oilTemp':
+        oilTemp.push(newMeasurement);
+        break;
+      case 'tubingPressure':
+        tubingPressure.push(newMeasurement);
+        break;
+      case 'waterTemp':
+        waterTemp.push(newMeasurement);
+        break;
+      case 'casingPressure':
+        casingPressure.push(newMeasurement);
+        break;
+      case 'injValveOpen':
+        injValveOpen.push(newMeasurement);
+        break;
+      case 'flareTemp':
+        flareTemp.push(newMeasurement);
+        break;
+      default:
+        console.log(newMeasurement.metric);
+    }
+    console.log(currentMetrics);
+  };
 
   useEffect(() => {
     if (error) console.log(error);
-    if (data) console.log(data);
+    if (data) {
+      updateCurrentMetrics(data.newMeasurement);
+    }
   }, [data, error]);
 
   return (
     <Card>
-      <CardHeader title={JSON.stringify(data || error)} />
+      <CardHeader title='Oil Temp' />
+      <ul>
+        {oilTemp.map((measurement) => <li>{measurement.value}</li>)}
+      </ul>
+
     </Card>
   );
 };
